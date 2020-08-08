ddc440c7d5bad673288f61c8349f4078f5d812f0
nekogokoro <nekogokoro024@gmail.com>
2020-08-08 13:04:23 +0900

    commit2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.initialize_on_precompile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -88,7 +89,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.compile = true
+  # config.assets.compile = true
   # config.assets.compile = tru
 
   # Inserts middleware to perform automatic connection switching.
