4f5040a04dc819261d6bdf64398498dd4affdf06
nekogokoro <nekogokoro024@gmail.com>
2020-08-11 13:57:15 +0900

    ログイン、新規登録機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
-
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday])
+   
   end
 
   
