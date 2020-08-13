ea9b27f10980ea22ccc1e8a0027da7e3f4d05f04
nekogokoro <nekogokoro024@gmail.com>
2020-08-13 11:36:30 +0900

    商品購入後のビューファイルの作成１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,25 @@ class ItemsController < ApplicationController
  
 
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title, :text, :price,:category,:status,:shipping_charges,:shipping_region, :day_until_shipping,).merge(user_id: current_user.id)
+  end
+
 end
