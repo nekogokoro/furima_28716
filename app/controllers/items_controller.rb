55f73571334740fa6f21f90e1857cad759dfcef7
nekogokoro <nekogokoro024@gmail.com>
2020-08-18 12:09:23 +0900

    before_actionの追加　エラーハンドリングの表記追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
+
+
+
+  before_action :set_artcle, only: [:show, :edit, :update,]
+
   
 
   def index
@@ -20,16 +25,15 @@ class ItemsController < ApplicationController
     end
   end
   def show
-   @article = Article.find(params[:id])
+  
   end
   def edit
-    
-    @article = Article.find(params[:id])
+   
   end
   def update
     
     
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       redirect_to item_path
     else
@@ -43,6 +47,11 @@ class ItemsController < ApplicationController
   def article_params
     params.require(:article).permit(:image, :title, :text, :price,:category_id,:status_id,:shipping_charge_id,:shipping_region_id, :day_until_shipping_id,).merge(user_id: current_user.id)
   end
+
+  def set_artcle 
+    @article = Article.find(params[:id])
+  end
+
   
 
 end
